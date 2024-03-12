6931619d0fb04d62cceefe359ac6deb4b6f4231a
Tomasz Marciniak <tomasz.marciniak@capgemini.com>
2024-03-12 15:11:50 +0100

    validation for login and registration

--- Mongo.Web/Controllers/AuthController.cs
+++ Mongo.Web/Controllers/AuthController.cs
@@ -47,7 +47,7 @@ namespace Mango.Web.Controllers
             }
             else
             {
-                ModelState.AddModelError("CustomError", responseDto.Message);
+                TempData["error"] = responseDto.Message;
                 return View(obj);
             }
         }
@@ -81,6 +81,10 @@ namespace Mango.Web.Controllers
                     TempData["success"] = "Ragistration Successful";
                     return RedirectToAction(nameof(Login));
                 }
+            }
+            else
+            {
+                TempData["error"] = result.Message;
             }
 
             var roleList = new List<SelectListItem>()
